8689bbbf1b06d9c6fbcf68a5fdeefa32155517e5
emforce <emforce@live.com>
2016-01-20 19:50:29 +0000

    laying the foundation for the process execution

--- modules/sniffer.py
+++ modules/sniffer.py
@@ -3,7 +3,7 @@ import socket
 # Our sniffer class that will do all our packet sniffing
 class sniffer():
     
-    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
     
     def __init__(self):
         print "Sniffer Initialized"
